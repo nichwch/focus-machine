7262d9ca5e6755c7a444b44c66ce74c00c6f22c1
nichwch <nichwch@gmail.com>
2021-02-19 21:20:01 -0800

    art

--- src/focusMachine.tsx
+++ src/focusMachine.tsx
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import mech from "./mech.png";
+import flare from "./flare.png";
 
 function FocusMachine(props: any) {
   const [goal, setGoal] = useState("");
   const [intervalValue, setIntervalValue] = useState("");
   const [intervalObject, setIntervalObject] = useState(null as any);
 
-  // clear interval on unmount
-  useEffect(() => {
-    return () => {
-      clearInterval(intervalObject);
-    };
-  }, [intervalObject]);
+  //   // clear interval on unmount
+  //   useEffect(() => {
+  //     return () => {
+  //       clearInterval(intervalObject);
+  //     };
+  //   }, [intervalObject]);
 
   const createInterval = () => {
     if (!("Notification" in window)) {
-      alert("This browser does not support desktop notification.");
+      alert("This browser does not support desktop notifications.");
     } else if (Notification.permission !== "granted") {
       Notification.requestPermission().then(function (permission) {
         // If the user accepts, let's create a notification
@@ -32,20 +34,21 @@ function FocusMachine(props: any) {
         body: "body test\nbody test",
       }
     );
-    // clearInterval(intervalObject);
+
     var newInterval = setInterval(() => {
       new Notification(`${goal}`);
     }, parseInt(intervalValue) * 1000);
+    clearInterval(intervalObject);
     setIntervalObject(newInterval);
   };
 
   const deleteInterval = () => {
+    clearInterval(intervalObject);
     setIntervalObject(null);
   };
   return (
     <MachineContainer>
       <MachineHeader>Focus Machine</MachineHeader>
-      {intervalObject ? "activate" : "not active"}
       <MachineInput
         placeholder="What do you want to work on?"
         onChange={(e) => {
@@ -65,9 +68,40 @@ function FocusMachine(props: any) {
       ) : (
         <MachineButton onClick={createInterval}>Start</MachineButton>
       )}
+      <TargetAcquiredText visible={intervalObject}>
+        TARGET ACQUIRED
+      </TargetAcquiredText>
+      <MechImage src={mech} />
+      <FlareImage src={flare} visible={intervalObject} />
     </MachineContainer>
   );
 }
+
+const TargetAcquiredText = styled.h3<{ visible: boolean }>`
+  color: red;
+  text-align: center;
+  margin: 20px 0px 0px 0px;
+  opacity: ${(props) => (props.visible ? 1 : 0)};
+`;
+
+const MechImage = styled.img`
+  height: 400px;
+  width: 400px;
+  display: block;
+  margin: auto;
+`;
+
+const FlareImage = styled.img<{ visible: boolean }>`
+  height: 400px;
+  width: 400px;
+  display: block;
+  margin: auto;
+
+  position: relative;
+  top: -400px;
+  opacity: ${(props) => (props.visible ? 1 : 0)};
+  transition: opacity 0.1s;
+`;
 
 const MachineHeader = styled.h1`
   text-align: center;
@@ -86,22 +120,17 @@ const MachineButton = styled.button`
 `;
 
 const MachineContainer = styled.div`
-  margin: 10vh auto 10vh auto;
+  margin: auto;
   padding: 20px;
-  width: 300px;
-  height: 300px;
+  width: 400px;
+  height: 400px;
 
-  border: 2px solid black;
-  border-radius: 30px;
   /* box-shadow: 0px 0px 10px 10px #aaaaaa inset, 5px 10px 10px 10px #aaaaaa; */
-  box-shadow: 0px 0px 10px 7.5px #e4e4e4;
 
-  @media only screen and (max-width: 600px) {
+  @media only screen and (max-width: 400px) {
     margin: 0px;
     width: calc(100% - 40px);
     height: 100%;
-    border: none;
-    box-shadow: none;
   }
 `;
 export default FocusMachine;
